700bd4b5926b50c49730796f947c5e6458f6d49f
Jaelani Mustafa <82elmustafa82@gmail.com>
2020-08-12 13:53:32 +0700

    Updates

---
+++ precache-manifest.566470ba925fc4c3f97d3d1eaf32d0a3.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3017d0add59faa5ff45a9c817118a98b",
+    "url": "/semantic-ui/index.html"
+  },
+  {
+    "revision": "7ce1625ab651595b842d",
+    "url": "/semantic-ui/static/css/2.27aa7f32.chunk.css"
+  },
+  {
+    "revision": "12f6125c62d2ef29dda9",
+    "url": "/semantic-ui/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7ce1625ab651595b842d",
+    "url": "/semantic-ui/static/js/2.827be7e5.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/semantic-ui/static/js/2.827be7e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12f6125c62d2ef29dda9",
+    "url": "/semantic-ui/static/js/main.d262c30b.chunk.js"
+  },
+  {
+    "revision": "f89226c98f606c20e300",
+    "url": "/semantic-ui/static/js/runtime-main.01f60ab0.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/semantic-ui/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/semantic-ui/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/semantic-ui/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/semantic-ui/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/semantic-ui/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/semantic-ui/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/semantic-ui/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/semantic-ui/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/semantic-ui/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/semantic-ui/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/semantic-ui/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/semantic-ui/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/semantic-ui/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/semantic-ui/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/semantic-ui/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/semantic-ui/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/semantic-ui/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
